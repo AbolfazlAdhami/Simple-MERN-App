7466fc93def042005fe14411b188c9c4c27ee168
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-10 02:33:48 +0330

    feat(middleware): done  HttpError Middleware and res error

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,13 @@ app.use((req, res, next) => {
   throw error;
 });
 
+app.use((error, req, res, next) => {
+  if (res.headerSent) return next(error);
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An not find this route" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
