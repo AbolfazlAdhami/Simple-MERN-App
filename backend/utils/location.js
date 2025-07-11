8b2d3b3f90d4277c37a2761420c6dbc208b57a0c
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-11 11:46:35 +0330

    feat(util): location convert to geocoding api setup

--- backend/utils/location.js
+++ backend/utils/location.js
@@ -3,10 +3,23 @@ const HttpError = require("../models/http-error");
 const api_key = process.env.GEOCODING_API_KEY;
 
 const getCoordsFromAddress = async (address = "20 W 34th St., New York, NY 10001, USA") => {
+  const options = {
+    method: "GET",
+    url: "https://address-from-to-latitude-longitude.p.rapidapi.com/geolocationapi",
+    params: {
+      address,
+    },
+    headers: {
+      "x-rapidapi-key": api_key,
+      "x-rapidapi-host": "address-from-to-latitude-longitude.p.rapidapi.com",
+    },
+  };
+
   if (!api_key) throw new HttpError("Could not Found Api KEy for Coords Data", 404);
 
-  const { data, status } = await axios.get(`https://geocode.maps.co/search?q=${address}&api_key=${api_key}`);
+  const { data, status } = await axios.request(options);
   console.log(data, status);
+  return data;
 };
 
 module.exports = getCoordsFromAddress;
