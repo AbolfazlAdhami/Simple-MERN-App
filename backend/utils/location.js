a2412954131a4561dea0381138f3360ed6683c5b
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-11 04:21:19 +0330

    chor(utils): location change address to geocoding setup

--- backend/utils/location.js
+++ backend/utils/location.js
@@ -1,8 +1,12 @@
 const axios = require("axios");
-// const api_key=
+const HttpError = require("../models/http-error");
+const api_key = process.env.GEOCODING_API_KEY;
 
-const getCoordsFromAddress=async(address)=>{
+const getCoordsFromAddress = async (address = "20 W 34th St., New York, NY 10001, USA") => {
+  if (!api_key) throw new HttpError("Could not Found Api KEy for Coords Data", 404);
 
+  const { data, status } = await axios.get(`https://geocode.maps.co/search?q=${address}&api_key=${api_key}`);
+  console.log(data, status);
+};
 
-
-}
+module.exports = getCoordsFromAddress;
