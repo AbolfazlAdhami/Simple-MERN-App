9a932c5e2cbaa49dd6bc341e0cb8b24cc600b7af
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-16 17:14:37 +0330

    feat(hook): setup and complete form reducer

--- frontend/src/hook/useForm.js
+++ frontend/src/hook/useForm.js
@@ -1,13 +1,57 @@
 import { useCallback, useReducer } from "react";
 
+// action.type 'INPUT_CHANGE' || 'SET_DATA'
 const formReducer = (state, action) => {
   switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = false;
+      for (const inputId in state.inputs) {
+        if (!state.inputs[inputId]) continue;
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && state.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+          isValid: formIsValid,
+        },
+      };
+    case "SET_DATA":
+      return {
+        inputs: action.inputs,
+        isValid: action.formIsValid,
+      };
+
     default:
       return { ...state };
   }
 };
 
 export const useForm = (initialInputs, initialFormValidity) => {
-    const []=useReducer()
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: initialInputs,
+    isValid: initialFormValidity,
+  });
+  const inputHanlder = useCallback((id, value, isValid) => {
+    dispatch({
+      type: "INPUT_CHANGE",
+      value,
+      isValid,
+      inputId: id,
+    });
+  }, []);
+  const setFormData = useCallback((inputData, formValidity) => {
+    dispatch({
+      type: "SET_DATA",
+      inputs: inputData,
+      formIsValid: formValidity,
+    });
+  }, []);
 
+  return { formState, inputHanlder, setFormData };
 };
