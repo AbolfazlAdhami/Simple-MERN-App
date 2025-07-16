f81d7174ae8c4d5a431ff8a15cd8cba7f5cdb71e
AbolfazlAdhami <aadhami880@gmail.com>
2025-07-16 15:21:09 +0330

    new: input components and reducer

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,5 +1,32 @@
-const Input = () => {
-  return <div></div>;
+import React, { useEffect, useReducer } from "react";
+import { validate } from "../../utils/validators";
+
+const inputeReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.value,
+        isValid: validate(action.value, action.validators),
+      };
+    case "TOUCH":
+      return {
+        ...state,
+        isTouched: true,
+      };
+    default:
+      return state;
+  }
+};
+
+const Input = ({ type, initialValue, initialValid }) => {
+  const [inputeState, dispatch] = useReducer(inputeReducer, {
+    value: initialValue || "",
+    isTouched: false,
+    isValid: initialValid || false,
+  });
+
+  return <div className=""></div>;
 };
 
 export default Input;
--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Input from "../components/ui/Input";
+import Button from "../components/ui/Button";
 
 function AuthPage() {
-  return <div className="text-red-600 text-2xl">AuthPage</div>;
+  return (
+    <section className="container flex justify-center items-center my-12 ">
+      <div className="md:w-2/5 w-full bg-slate-700 p-4 flex items-center justify-between rounded-lg">
+        <h1 className="font-bold text-xl">Login Form</h1>
+        <form>
+          <Input />
+          <Button />
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default AuthPage;
