89fa8fb5e9ee7a2bcf60606bda4ceb7a71d2f387
AbolfazlAdhami <aadhami880@gmail.com>
2025-12-05 21:13:07 +0330

    feat: user controller done.

--- backend/controller/user-controller.js
+++ backend/controller/user-controller.js
@@ -1,58 +1,80 @@
 const { validationResult } = require("express-validator");
 const uuid = require("uuid");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 const HttpError = require("../models/http-error");
 
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Abolfal Adhami",
-    email: "abolfazl@gmail.coms",
-    password: "password123",
-  },
-  {
-    id: "u2",
-    name: "Ali Adhami",
-    email: "ali@gmail.coms",
-    password: "password123",
-  },
-];
+const getUsersHandler = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find();
+  } catch (error) {
+    return next(new HttpError("Fetching users failed ,please try again later.", 500));
+  }
 
-const getUsersHandler = (req, res) => {
-  res.json({ data: DUMMY_USERS });
+  res.json({ users: users.map((user) => user.toObject({ getter: true })) });
 };
 
 const createUserHandler = async (req, res, next) => {
   const errors = validationResult(req);
-  if (!errors.isEmpty()) throw new HttpError("Invalid inputes passed , Please check your data", 422);
+  if (!errors.isEmpty()) throw new HttpError("Invalid inputs passed , Please check your data", 422);
 
   const { name, email, password } = req.body;
 
-  const hasUser = DUMMY_USERS.find((user) => user.email === email);
-  if (hasUser) throw new HttpError("Could not create user , Email already exists", 422);
-  const salt = await bcrypt.genSalt(10);
+  try {
+    const hasUser = await User.findOne({ email });
 
-  const createdUser = {
-    id: uuid.v4(),
-    name,
-    email,
-    password: await bcrypt.hash(password, salt),
-  };
+    if (hasUser) throw new HttpError("Could not create user , Email already exists", 422);
 
-  DUMMY_USERS.push(createdUser);
+    const salt = await bcrypt.genSalt(10);
 
-  return res.status(201), json({ message: "User Account Created!", data: createdUser });
+    const createdUser = new User({
+      name,
+      email,
+      image: req.file.path,
+      password: await bcrypt.hash(password, salt),
+      places: [],
+    });
+
+    await createdUser.save();
+    const token = jwt.sign({ userId: createdUser.id, email: createdUser.email }, "supersecret_dont_share", { expiresIn: "1h" });
+
+    return res.status(201), json({ message: "User Account Created!", data: createdUser, token });
+  } catch (err) {
+    return new HttpError("Could not create user, please try again.", 500);
+  }
 };
 
-const loginUserHandler = (req, res, next) => {
+const loginUserHandler = async (req, res, next) => {
   const { email, password } = req.body;
 
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser) throw new HttpError("Could not identify user, ", 401);
-  if (identifiedUser.password !== password) throw new HttpError("User Password not seem to be wrong", 401);
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) throw new HttpError("Could not identify user, ", 401);
 
-  res.json({ message: "Logged in!" });
+    let isValidPassword = false;
+
+    try {
+      isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (error) {
+      return next(new HttpError("Could not log you in, please check your credentials and try again.", 500));
+    }
+
+    if (!isValidPassword) throw new HttpError("Invalid password, could not log you in.", 401);
+
+    let token;
+    try {
+      token = jwt.sign({ userId: existingUser.id, email: existingUser.email }, "supersecret_dont_share", { expiresIn: "1h" });
+    } catch (error) {
+      return next(new HttpError("Logging in failed, please try again later.", 500));
+    }
+
+
+    return res.json({ userId: existingUser.id, email: existingUser.email, token });
+  } catch (error) {
+    return next(new HttpError("Logging in failed, please try again later.", 500));
+  }
 };
 
-module.exports = { createUserHandler, loginUserHandler,getUsersHandler };
+module.exports = { createUserHandler, loginUserHandler, getUsersHandler };
